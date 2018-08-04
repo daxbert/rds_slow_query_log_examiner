1314122754ab8a191c0e5d15006805ce1bfd4dae
Dax Eckenberg <dax@chegg.com>
2018-08-03 17:22:54 -0700

    Fixes #11: Add .ps1 powershell files for windows support

---
+++ clean.ps1
@@ -0,0 +1,6 @@
+#! POWERSHELL
+
+Remove-Item ./rootfs/www/.bash_history -ErrorAction SilentlyContinue
+Remove-Item ./rootfs/apps/rds_slow_query_log_examiner/www/*.data -ErrorAction SilentlyContinue
+Remove-Item ./rootfs/apps/rds_slow_query_log_examiner/www/report_*/ -Recurse -Force -ErrorAction SilentlyContinue
+
