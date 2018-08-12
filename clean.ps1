e70777d9e73c4f481871c087ab905c9c65344abe
daxbert <deckenberg@gmail.com>
2018-08-12 00:54:11 -0700

    Cleanup changes for Windows and Linux
    Remove .pyc files
    Remove __pycache__ created by PyCharm

--- clean.ps1
+++ clean.ps1
@@ -1,7 +1,8 @@
 #! POWERSHELL
 
 Remove-Item ./rootfs/www/.bash_history -ErrorAction SilentlyContinue
+Remove-Item ./rootfs/apps/rds_slow_query_log_examiner/www/*.pyc -Force -ErrorAction SilentlyContinue 
 Remove-Item ./rootfs/apps/rds_slow_query_log_examiner/www/*.data -ErrorAction SilentlyContinue
 Remove-Item ./rootfs/apps/rds_slow_query_log_examiner/www/*.data.* -ErrorAction SilentlyContinue
 Remove-Item ./rootfs/apps/rds_slow_query_log_examiner/www/report_*/ -Recurse -Force -ErrorAction SilentlyContinue
-
+Remove-Item ./rootfs/apps/rds_slow_query_log_examiner/www/__pycache__ -Recurse -Force -ErrorAction SilentlyContinue
--- clean.sh
+++ clean.sh
@@ -9,4 +9,5 @@ rm -rf ./rootfs/root/.cache || true
 rm ./rootfs/root/.bash_history || true
 rm ./rootfs/www/.bash_history || true
 rm ./rootfs/apps/rds_slow_query_log_examiner/www/*.data || true
+rm ./rootfs/apps/rds_slow_query_log_examiner/www/*.pyc || true
 rm -rf ./rootfs/apps/rds_slow_query_log_examiner/www/report_*/ || true
