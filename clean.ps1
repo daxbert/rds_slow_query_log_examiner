ed62e50daf9abcf5c31f2025eebf5c61139a9002
daxbert <deckenberg@gmail.com>
2018-08-12 00:45:45 -0700

    Add more files to clean.ps1 script
    Ensure that the various logEntriesCache data file are purged
    *.data.* covers *.data.bak, *.data.dat, *.data.dir
    First observed on windows hosts, will adjust clean.sh if/when
    these files appear on linux test runs

--- clean.ps1
+++ clean.ps1
@@ -2,5 +2,6 @@
 
 Remove-Item ./rootfs/www/.bash_history -ErrorAction SilentlyContinue
 Remove-Item ./rootfs/apps/rds_slow_query_log_examiner/www/*.data -ErrorAction SilentlyContinue
+Remove-Item ./rootfs/apps/rds_slow_query_log_examiner/www/*.data.* -ErrorAction SilentlyContinue
 Remove-Item ./rootfs/apps/rds_slow_query_log_examiner/www/report_*/ -Recurse -Force -ErrorAction SilentlyContinue
 
