6bca14e6ffdcff5a7e32a56f9f80c0ecbdb55073
daxbert <deckenberg@gmail.com>
2018-09-06 18:01:23 -0700

    Issue #36:  First working tests
    Actually update .dockerignore
    
    Add two tests to validate that the app
    redirects when called w/o credentials

--- rootfs/apps/rds_slow_query_log_examiner/tests/test_app.py
+++ rootfs/apps/rds_slow_query_log_examiner/tests/test_app.py
@@ -1,3 +1,5 @@
+import pprint
+
 import pytest
 from rds_slow_query_log_examiner import create_app
 
@@ -10,6 +12,19 @@ def app():
     yield app
 
 @pytest.fixture
-def client():
+def client(app):
     return app.test_client()
+
+
+def test_nouser_homepage_redirects(client):
+    rv = client.get('/')
+    assert rv.status_code == 302
+    assert "/credentials?redirect=" in rv.location
+    pprint.pprint(rv.location)
+
+def test_nouser_regions_redirects(client):
+    rv = client.get('/regions')
+    assert rv.status_code == 302
+    assert "/credentials?redirect=" in rv.location
+    pprint.pprint(rv.location)
 
